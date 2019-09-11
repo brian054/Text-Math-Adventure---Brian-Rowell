27ead98089aad0ff3bddd272cfcc981887924118
brian054 <brian.rowell54@gmail.com>
2019-09-10 22:59:08 -0400

    Add project files.

---
+++ Text Math Adventure - Brian Rowell/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	string NAME;
+
+	cout << "You awaken, you are in a small pool of water in a dark cave...." << endl;
+	cout << "\"Where am I?.....\"" << endl;
+	cout << "You stand up, you think for a second...." << endl;
+	cout << "\"The hell is my name...?\"" << endl;
+	cout << "Please enter your name: ";
+	cin >> NAME;
+	cout << "\"Oh yeah I'm " << NAME << ".\"" << endl;
+
+	system("pause");
+	return 0;
+}
